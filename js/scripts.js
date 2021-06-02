79c59aff9e0ffe09b3deff0082cd76ee76e4b033
Shoshana Newman <sho.newman@gmail.com>
2021-06-02 14:59:25 -0700

    Add form functionality to reproduce inputs in receiptdiv
    Co-authored-by: Allister <allisterkmoon@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-function userSubmittedInformation (event) {
-  console.log('doing stuff here!')
+function userSubmittedInformation (event) {  
+  const inputFirstName = $('#firstName').val();
+  const inputLastName = $('#lastName').val();
+  const inputAddress = $('#address').val();
+  const inputCity = $('#city').val();
+  const inputState = $('#state').val();
+  const inputZip = $('#zipcode').val();
+
+  $('#receipt .firstName').text(inputFirstName);
+  $('#receipt .lastName').text(inputLastName);
+  $('#receipt .address').text(inputAddress);
+  $('#receipt .city').text(inputCity);
+  $('#receipt .state').text(inputState);
+  $('#receipt .zipcode').text(inputZip);
+
+  event.preventDefault();  
 }
 
 $(document).ready(function () {
-  $(form).submit(userSubmittedInformation)
+  $('form').submit(userSubmittedInformation);
 })
